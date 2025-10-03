ec6769c5176aa6abf8d5e5cad2e07dfb01903c47
ssitar583 <soundaryaa_sitaram@comcast.com>
2025-10-03 12:51:56 +0530

    Update NetworkSetup.cpp

--- rdkPlugins/Networking/source/NetworkSetup.cpp
+++ rdkPlugins/Networking/source/NetworkSetup.cpp
@@ -175,6 +175,10 @@ std::vector<Netfilter::RuleSet> constructBridgeRules(const std::shared_ptr<Netfi
                 "POSTROUTING -s %y ! -d %y ! -o " BRIDGE_NAME " -p tcp -j MASQUERADE --to-ports 1024-65535",
                 "POSTROUTING -s %y ! -d %y ! -o " BRIDGE_NAME " -p udp -j MASQUERADE --to-ports 1024-65535",
                 "POSTROUTING -s %y ! -d %y ! -o " BRIDGE_NAME " -j MASQUERADE"
+
+                "POSTROUTING -s 100.64.11.0/24 -d 100.64.11.1 -p tcp --dport 8016 -j SNAT --to-source 127.0.0.1 -m comment --comment container-to-host-hostns"
+                "POSTROUTING -s 100.64.11.0/24 -d 100.64.11.1 -p tcp --dport 8017 -j SNAT --to-source 127.0.0.1 -m comment --comment container-to-host-hostns"
+                "POSTROUTING -s 100.64.11.0/24 -d 100.64.11.1 -p tcp --dport 8443 -j SNAT --to-source 127.0.0.1 -m comment --comment container-to-host-hostns"
             }
         },
         {
