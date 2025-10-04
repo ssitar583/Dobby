61a0465e37e97d65f069fb98433c34f5e8b302b3
ssitar583 <soundaryaa_sitaram@comcast.com>
2025-10-04 10:31:37 +0530

    Update NetworkSetup.cpp

--- rdkPlugins/Networking/source/NetworkSetup.cpp
+++ rdkPlugins/Networking/source/NetworkSetup.cpp
@@ -208,6 +208,12 @@ std::vector<Netfilter::RuleSet> constructBridgeRules(const std::shared_ptr<Netfi
         appendNatRules->second.emplace_back("POSTROUTING -s 100.64.11.0/24 -p tcp --dport 8016 -j SNAT --to-source 127.0.0.1 -m comment --comment container-to-host-hostns");
         appendNatRules->second.emplace_back("POSTROUTING -s 100.64.11.0/24 -p tcp --dport 8017 -j SNAT --to-source 127.0.0.1 -m comment --comment container-to-host-hostns");
         appendNatRules->second.emplace_back("POSTROUTING -s 100.64.11.0/24 -p tcp --dport 8443 -j SNAT --to-source 127.0.0.1 -m comment --comment container-to-host-hostns");
+        appendNatRules->second.emplace_back("PREROUTING -s 100.64.11.0/24 -p tcp --dport 8016 -j SNAT --to-source 127.0.0.1 -m comment --comment container-to-host-hostns");
+        appendNatRules->second.emplace_back("PREROUTING -s 100.64.11.0/24 -p tcp --dport 8017 -j SNAT --to-source 127.0.0.1 -m comment --comment container-to-host-hostns");
+        appendNatRules->second.emplace_back("PREROUTING -s 100.64.11.0/24 -p tcp --dport 8443 -j SNAT --to-source 127.0.0.1 -m comment --comment container-to-host-hostns");
+        appendNatRules->second.emplace_back("INPUT -s 100.64.11.0/24 -p tcp --dport 8016 -j SNAT --to-source 127.0.0.1 -m comment --comment container-to-host-hostns");
+        appendNatRules->second.emplace_back("INPUT -s 100.64.11.0/24 -p tcp --dport 8017 -j SNAT --to-source 127.0.0.1 -m comment --comment container-to-host-hostns");
+        appendNatRules->second.emplace_back("INPUT -s 100.64.11.0/24 -p tcp --dport 8443 -j SNAT --to-source 127.0.0.1 -m comment --comment container-to-host-hostns");
 
         // reject with "icmp-port-unreachable" if not ACCEPTed by now
         Netfilter::RuleSet::iterator appendFilterRules = appendRuleSet.find(Netfilter::TableType::Filter);
